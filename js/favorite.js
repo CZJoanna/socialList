a582335efec16bd22e537738df4ebd2858439009
CZJoanna <jyunchen199512@gmail.com>
2021-07-18 18:41:38 +0800

    add rwd of small devices

--- js/favorite.js
+++ js/favorite.js
@@ -7,94 +7,151 @@ const users_per_page = 12;
 // fav list array
 const favUserArray = JSON.parse(localStorage.getItem("favoriteUsers"));
 const total_pages = Math.ceil(favUserArray.length / users_per_page);
+let searchArray = [];
 
 // 渲染畫面
-renderUserList(favUserArray);
+renderUserList(getUserByPage(1));
+
+// 渲染分頁器
+myPaginator(total_pages);
+
 // 畫面進入會先計算現在local storage有幾個Fav資料
 favCount.innerHTML = favUserArray.length;
 
+// 函式:
+function getInputValue() {
+  if (!inputContent.value.trim().length) {
+    searchArray = [];
+    renderUserList(getUserByPage(1));
+  }
+  searchArray = favUserArray.filter((user) => {
+    return (user.surname + "" + user.name)
+      .toLowerCase()
+      .includes(inputContent.value.trim().toLowerCase());
+  });
+
+  if (searchArray.length) {
+    const searchTotalPages = Math.ceil(searchArray.length / users_per_page);
+    myPaginator(searchTotalPages);
+    renderUserList(getUserByPage(1));
+  } else {
+    friendList.innerText =
+      "Sorry,we don't find any people matching this search.";
+    myPaginator(0);
+  }
+
+}
+
 //函式:回傳特定筆數的資料(陣列結構)-->用於放入
 function getUserByPage(page) {
-    const startIndex = (page - 1) * users_per_page;
-    return favUserArray.slice(startIndex, startIndex + users_per_page);
+  const array = searchArray.length ? searchArray : favUserArray;
+  const startIndex = (page - 1) * users_per_page;
+  return array.slice(startIndex, startIndex + users_per_page);
+}
+
+//函式:
+function myPaginator(totalPages) {
+  let page = "";
+  for (let i = 1; i <= totalPages; i++) {
+    page += `<li class="page-nav__item">
+        <a class="page-nav__link" href="#" data-page="${i}">${i}</a>
+      </li>`;
+  }
+  paginator.innerHTML = page;
 }
 
 // 函式:製作好li template 放入ul渲染在畫面上
 function renderUserList(array) {
-    let dataCard = "";
-    array.forEach((data) => {
-        const id = data.id;
-        const name = data.name;
-        const surName = data.surname;
-        const image = data.avatar;
-        dataCard += `
-        <div class="friend" id="userCard">
-        <img src="${image}" class="friend__img" />
-        <h3 class="friend__name">${name}&nbsp;${surName}</h3>
-        <button
-          data-id="${id}"
-          ;
-          data-toggle="modal"
-          data-target="#user-modal"
-          class="f-btn f-btn--orange friend__seemore show-userCard"
-        >
-          See more
-        </button>
-        <button class="f-btn f-btn--grey friend__add btn-add-favorite" data-id="${id}">
-          -
-        </button>
-      </div>
-              `;
-    });
-    friendList.innerHTML = dataCard;
+  let dataCard = "";
+  array.forEach((data) => {
+    const { id, name, surname, avatar } = data;
+
+    dataCard += `
+      <div class="friend" id="userCard">
+       <img src="${avatar}" class="friend__img" />
+       <h3 class="friend__name">${name}&nbsp;${surname}</h3>
+       <button
+         data-id="${id}"
+         ;
+         data-toggle="modal"
+         data-target="#user-modal"
+         class="f-btn f-btn--orange friend__seemore show-userCard"
+      >
+         See more
+       </button>
+       <button class="f-btn f-btn--grey friend__add add-to-favorite " data-id="${id}">
+         -
+       </button>
+     </div>
+       `;
+  });
+  friendList.innerHTML = dataCard;
 }
 
 // 刪除最愛
 function deleteFromFav(favId) {
-    if (!favUserArray) return;
-    const selectedUser = favUserArray.find((item) => {
-        return item.id == favId;
-    });
-    // console.log(selectedUser);
+  if (!favUserArray) return;
+  const selectedUser = favUserArray.find((item) => {
+    return item.id == favId;
+  });
+  // console.log(selectedUser);
 
-    // 找出要刪除的物件在陣列的第幾個索引
-    let userIndex = favUserArray.indexOf(selectedUser);
-    if (userIndex == -1) return;
-    // console.log(userIndex);
-    favUserArray.splice(userIndex, 1);
-    //存回 local storage
-    localStorage.setItem("favoriteUsers", JSON.stringify(favUserArray));
-    favCount.innerHTML = favUserArray.length;
-    // 重新渲染頁面
-    renderUserList(favUserArray);
+  // 找出要刪除的物件在陣列的第幾個索引
+  let userIndex = favUserArray.indexOf(selectedUser);
+  if (userIndex == -1) return;
+  // console.log(userIndex);
+  favUserArray.splice(userIndex, 1);
+  //存回 local storage
+  localStorage.setItem("favoriteUsers", JSON.stringify(favUserArray));
+  favCount.innerHTML = favUserArray.length;
+  // 重新渲染頁面
+  renderUserList(favUserArray);
 }
 
-// 監聽卡片點擊事件 1.點擊More按鈕獲取個人資料   2.點擊愛心按鈕將user從最愛清單中移除
+/*****
+ event addEventListener
+ *****/
+// 監聽卡片點擊事件 1.點擊More按鈕獲取個人資料   2.點擊-按鈕將user從最愛清單中移除
 friendList.addEventListener("click", (e) => {
-    //
-    if (e.target.matches(".show-userCard")) {
-        const userName = document.querySelector("#show-modal-name");
-        const userAge = document.querySelector("#show-modal-age");
-        const userBirth = document.querySelector("#show-modal-birth");
-        const userEmail = document.querySelector("#show-modal-email");
-        const userImage = document.querySelector("#show-modal-pic");
-        // 取得每個button上的可辨識id編號
-        const selectedId = e.target.dataset.id;
-        let selectedPersonInfo = favUserArray.find(
-            (item) => item.id == selectedId
-        );
+  // console.log(e.target);
+  if (e.target.matches(".show-userCard")) {
+    const selectedId = parseInt(e.target.dataset.id);
+    const userName = document.querySelector("#show-modal-name");
+    const userAge = document.querySelector("#show-modal-age");
+    const userBirth = document.querySelector("#show-modal-birth");
+    const userEmail = document.querySelector("#show-modal-email");
+    const userImage = document.querySelector("#show-modal-pic");
 
-        userName.innerText =
-            selectedPersonInfo.name + " " + selectedPersonInfo.surname;
-        userAge.innerText = selectedPersonInfo.age + " years old";
-        userBirth.innerText = selectedPersonInfo.birthday;
-        userImage.innerHTML = `
-          <img src="${selectedPersonInfo.avatar}" alt="圖片">
-          `;
-        userEmail.innerText = selectedPersonInfo.email;
-    } else if (e.target.matches(".btn-add-favorite")) {
-        let favId = e.target.dataset.id;
-        console.log(favId);
-        deleteFromFav(favId);
-    }
+    const userInfo = favUserArray.find((item) => item.id === selectedId); //特定資料物件
+    userName.innerText = "Name: " + userInfo.name + " " + userInfo.surname;
+    userAge.innerText = "Birth: " + userInfo.age + " years old";
+    userBirth.innerText = "Age: " + userInfo.birthday;
+    userImage.innerHTML = `
+                    <img class='user-info__img' src="${userInfo.avatar}" alt="圖片">
+                    `;
+    userEmail.innerText = userInfo.email;
+  } else if (e.target.matches(".add-to-favorite")) {
+    const favId = parseInt(e.target.dataset.id);
+    deleteFromFav(favId);
+  }
+});
+
+//監聽分頁器
+paginator.addEventListener("click", (e) => {
+  if (e.target.matches(".page-nav__link")) {
+    const page = e.target.dataset.page;
+    renderUserList(getUserByPage(page));
+  }
+});
+
+//監聽搜尋input
+inputContent.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getInputValue();
+  }
+});
+
+//監聽搜尋btn
+getValBtn.addEventListener("click", (e) => {
+  getInputValue();
 });
--- js/index.js
+++ js/index.js
@@ -12,14 +12,13 @@ const paginator = document.querySelector("#paginator");
 const inputContent = document.querySelector("#inputContent");
 const getValBtn = document.querySelector("#getValBtn");
 const favCount = document.querySelector("#favCount");
-const viewButton = document.querySelector("#viewButton");
 
 // Data and Variables
 // 如果favoriteUsers裡面是空的，則值會回傳null，null==false
 const myFriendList = JSON.parse(localStorage.getItem("favoriteUsers")) || [];
 const dataArray = [];
 let filterArray = [];
-let mode = "card";
+
 
 /* functions
  *****************************************/
@@ -31,56 +30,66 @@ function getUserByPage(page) {
   return data.slice(startIndex, startIndex + users_per_page);
 }
 
-// 函式:製作好li template 放入ul渲染在畫面上
+// 函式:製作friend card template 
 function renderUserList(array) {
   let dataCard = "";
   array.forEach((data) => {
-    const id = data.id;
-    const name = data.name;
-    const surName = data.surname;
-    const image = data.avatar;
+    const { id , name , surname , avatar } = data;
+  
     dataCard += `
     <div class="friend" id="userCard">
-    <img src="${image}" class="friend__img" />
-    <h3 class="friend__name">${name}&nbsp;${surName}</h3>
-    <button
-      data-id="${id}"
-      ;
-      data-toggle="modal"
-      data-target="#user-modal"
-      class="f-btn f-btn--orange friend__seemore show-userCard"
+     <img src="${avatar}" class="friend__img" />
+     <h3 class="friend__name">${name}&nbsp;${surname}</h3>
+     <button
+       data-id="${id}"
+       ;
+       data-toggle="modal"
+       data-target="#user-modal"
+       class="f-btn f-btn--orange friend__seemore show-userCard"
     >
-      See more
-    </button>
-    <button class="f-btn f-btn--grey friend__add btn-add-favorite " data-id="${id}">
-      +
-    </button>
-  </div>
+       See more
+     </button>
+     <button class="f-btn f-btn--grey friend__add add-to-favorite " data-id="${id}">
+       +
+     </button>
+   </div>
      `;
   });
   friendList.innerHTML = dataCard;
-  // showRenderMode();
+
 }
+
+
 // 函式:分頁器渲染
 function myPaginator(totalPages) {
   let page = "";
   for (let i = 1; i <= totalPages; i++) {
-    page += `<li class="page-item"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+    page += `
+              <li class="page-nav__item">
+                <a class="page-nav__link" href="#" data-page="${i}">${i}</a>
+              </li>
+            `;
   }
   paginator.innerHTML = page;
 }
+
+
 // 函式: 抓取input值
 function getInputValue() {
   let inputVal = inputContent.value;
-  if (inputVal.length) {
+  if (!inputContent.value.trim().length) {
+    searchArray = [];
+    renderUserList(getUserByPage(1));
+  }
+  
     // 篩選符合條件的物件放入陣列->filterArray
     //string.includes()方法
     // 把要比對的字串全部變小寫 toLowerCase()
     //函式:回傳特定筆數的資料(陣列結構)
 
     // 每次的搜尋，都會重新產生一個新的filterArray，但dataArray的內容不變
-    filterArray = dataArray.filter((item) => {
-      return (item.surname + " " + item.name)
+    filterArray = dataArray.filter((user) => {
+      return (user.surname + " " + user.name)
         .toLowerCase()
         .includes(inputVal.trim().toLowerCase());
     });
@@ -95,12 +104,7 @@ function getInputValue() {
         "Sorry,we don't find any people matching this search.";
       myPaginator(0);
     }
-    // 清空輸入的內容
-    inputContent.value = "";
-  } else {
-    alert("the searching info can not be empty!");
   }
-}
 
 // 加入最愛
 // 0.設一個全域變數空陣列:myFriendList，拿來存放被新增的摯友資料
@@ -111,7 +115,7 @@ function addToFavorite(favId) {
   const favUser = dataArray.find((item) => {
     return item.id === favId;
   });
-  console.log(typeof favUser);
+  // console.log(typeof favUser);
 
   if (myFriendList.some((item) => item.id === favId)) {
     return alert("nonono you've choosed");
@@ -121,36 +125,15 @@ function addToFavorite(favId) {
   favCount.innerHTML = myFriendList.length; // 重新渲染收藏好友的個數
 }
 
-// 切換瀏覽模式
-// function showRenderMode() {
-//     const userCard = document.querySelectorAll("#userCard");
-//     const cardBtn = document.querySelector('[data-modal="card"]');
-//     const listBtn = document.querySelector('[data-modal="list"]');
-//     if (mode === "card") {
-//         cardBtn.classList.add("colorBrown");
-//         listBtn.classList.remove("colorBrown");
-//         friendList.className = "friends";
-//         userCard.forEach((item) => {
-//             item.className = "friend";
-//         });
-//     } else if (mode === "list") {
-//         listBtn.classList.add("colorBrown");
-//         cardBtn.classList.remove("colorBrown");
-//         friendList.className = "friendContainerByList";
-//         userCard.forEach((item) => {
-//             item.className = "friendCardByList";
-//         });
-//     }
-// }
-
 /* Event Listeners
  *****************************************/
 
-// 分頁器掛監聽器-點擊
+// 分頁器-點擊監聽-
 // getUserByPage:根據被點擊的頁數，取出特定電影資料
 // renderUserList:將資料渲染在畫面上
 paginator.addEventListener("click", (e) => {
-  if (e.target.matches(".page-link")) {
+  console.log(e.target);
+  if (e.target.matches(".page-nav__link")) {
     const selectedPage = e.target.dataset.page;
     renderUserList(getUserByPage(selectedPage));
   }
@@ -158,56 +141,49 @@ paginator.addEventListener("click", (e) => {
 
 // 卡片掛監聽器-點擊
 //1.點擊More按鈕獲取個人資料
-//2.點擊愛心按鈕將user加入最愛清單
+//2.點擊+按鈕將user加入最愛清單
 friendList.addEventListener("click", (e) => {
+  // console.log(e.target);
   if (e.target.matches(".show-userCard")) {
+    const selectedId = parseInt(e.target.dataset.id);
     const userName = document.querySelector("#show-modal-name");
     const userAge = document.querySelector("#show-modal-age");
     const userBirth = document.querySelector("#show-modal-birth");
     const userEmail = document.querySelector("#show-modal-email");
     const userImage = document.querySelector("#show-modal-pic");
-    const selectedId = parseInt(e.target.dataset.id);
-    const PersonInfo = dataArray.find((item) => item.id === selectedId);
-    userName.innerText = PersonInfo.name + " " + PersonInfo.surname;
-    userAge.innerText = PersonInfo.age + " years old";
-    userBirth.innerText = PersonInfo.birthday;
+   
+    const userInfo = dataArray.find((item) => item.id === selectedId); //特定資料物件
+    userName.innerText = "Name: " + userInfo.name + " " + userInfo.surname;
+    userAge.innerText =  "Birth: " + userInfo.age + " years old";
+    userBirth.innerText = "Age: " + userInfo.birthday;
     userImage.innerHTML = `
-                  <img src="${PersonInfo.avatar}" alt="圖片">
+                  <img class='user-info__img' src="${userInfo.avatar}" alt="圖片">
                   `;
-    userEmail.innerText = PersonInfo.email;
-  } else if (e.target.matches(".btn-add-favorite")) {
+    userEmail.innerText = userInfo.email;
+  } else if (e.target.matches(".add-to-favorite")) {
     const favId = parseInt(e.target.dataset.id);
     addToFavorite(favId);
   }
 });
 
-// 搜尋按鈕掛監聽器-點擊
+// 搜尋按鈕-點擊監聽-
 getValBtn.addEventListener("click", () => {
   getInputValue();
 });
 
-// 搜尋盒子掛監聽器-Enter
+// 搜尋盒子-鍵盤Enter監聽-
 inputContent.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     getInputValue();
   }
 });
 
-// 對顯示模式區塊掛監聽器-點擊
-// viewButton.addEventListener("click", (e) => {
-//     if (e.target.matches(".fa-th")) {
-//         mode = "card";
-//     } else if (e.target.matches(".fa-bars")) {
-//         mode = "list";
-//     }
-//     showRenderMode();
-// });
-
 // 從API抓取資料
 axios
   .get(base_url + friends_url)
   .then((response) => {
-    dataArray.push(...response.data.results); //ES新語法 展開運算子
+    console.log(response.data.results) // target array
+    dataArray.push(...response.data.results); //ES新語法 展開運算子 （把陣列的[]拿掉的意思）
     const total_pages = Math.ceil(dataArray.length / users_per_page); //資料總筆數/每頁呈現筆數
     renderUserList(getUserByPage(1)); //渲染資料(首頁)
     myPaginator(total_pages); //分頁器
